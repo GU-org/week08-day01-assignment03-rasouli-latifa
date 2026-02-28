549f9e09f8bd9a2097fb37bb12f0a854ab38fccb
Latifa Rasouli <latifarasouli4@gmail.com>
2026-02-28 21:43:50 +0430

    assignment 3 week 8

--- Week08/Day01/Assignment03/index.js
+++ Week08/Day01/Assignment03/index.js
@@ -1,11 +1,14 @@
 function bmiCalc(w,h){
-    let bmi
+
+    let bmi = w / (h * h);
 
     return bmi;
 
 }
 
-var weight=prompt("Please enter you weight in kg");
-var height=prompt("Please enter your height in m");
-let bmi=bmiCalc(weight,height);
-alert("Your bmi is "+bmi);
+var weight = prompt("Please enter you weight in kg");
+var height = prompt("Please enter your height in m");
+
+let bmi = bmiCalc(weight,height);
+
+alert("Your bmi is " + bmi);
